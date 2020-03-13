20d5895db7b54489bf72dea09b42e5d62920c544
kaiomudkt <kaiomudkt@gmail.com>
2020-03-13 19:01:22 -0400

    configurando c-p-t para ativar api rest e apelindando 'pet'

--- custom-post-type/cria-custom-post-type-pet.php
+++ custom-post-type/cria-custom-post-type-pet.php
@@ -48,6 +48,8 @@ function pet_post_type() {
 		'public'                => true,
 		'show_ui'               => true,
 		'show_in_menu'          => true,
+		'show_in_rest'			=> true,
+		'rest_base'          	=> 'Pets',
 		'menu_position'         => 5,
 		'show_in_admin_bar'     => true,
 		'show_in_nav_menus'     => true,
